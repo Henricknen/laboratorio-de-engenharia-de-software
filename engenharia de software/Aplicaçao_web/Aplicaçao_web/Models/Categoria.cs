6d8ee936e4b842f7c41e09ab1e78f23eb1eed151
Luis Henrique Silva Ferreira <l.henrick@live.com>
2022-02-25 18:35:25 -0300

    aula passada

---
+++ engenharia de software/Aplicaçao_web/Aplicaçao_web/Models/Categoria.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Aplicaçao_web.Models
+{
+    public class Categoria
+    {
+        [Key]
+        public int Id { get; set; }         
+        public string Descricao { get; set; }
+
+    }
+}
---
+++ engenharia de software/Aplicaçao_web/Aplicaçao_web/Models/Contexto.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Aplicaçao_web.Models
+{
+    public class Contexto : DbContext
+    {
+        public Contexto(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Categoria> Categorias { get; set; }
+
+
+    }
+}
